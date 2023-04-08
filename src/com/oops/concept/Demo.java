e98aeab3ec49d9b9b3046c89f0a2c93a56073709
unknown <santoshpr84@gmal.com>
2023-04-08 13:57:53 +0530

    first commit for sample java code

---
+++ src/com/oops/concept/Demo.java
@@ -0,0 +1,42 @@
+package com.oops.concept;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		Dog d = new Dog();
+		d.barking();
+		d.eating();
+
+	}
+
+}
+
+class Dog{
+	
+	String breed = "dobermandog";
+	String name = "puppy";
+	String color = "white";
+	
+	public void barking() {
+		System.out.println(breed+" is barking");
+	}
+	
+	public void eating() {
+		System.out.println(breed+" is eating");
+	}
+	
+	public void sleeping() {
+		System.out.println(breed+" is sleeping");
+	}
+	
+	public void walking() {
+		System.out.println(breed+" is walking");
+	}
+	
+	public void running() {
+		System.out.println(breed+" is running");
+		System.out.println("-----------------------------------------");
+	}
+	
+}
